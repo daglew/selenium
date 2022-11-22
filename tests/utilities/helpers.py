409ee56b7a0afc883e39bb1f3822dad2a47e94dd
daglew <daga122611@gmail.com>
2022-11-22 23:53:14 +0100

    working with elements

--- tests/utilities/helpers.py
+++ tests/utilities/helpers.py
@@ -1,9 +1,29 @@
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from paths import Paths
 
 
 class Helpers:
     def __init__(self, driver):
         self.driver = driver
+
+    def enter_by_type(self, locator_type):
+        locator_type = locator_type.lower()
+        if locator_type == "id":
+            return By.ID
+        elif locator_type == "xpath":
+            return By.XPATH
+        else:
+            print("The locator is invalid.")
+        return False
+
+    def by_element(self, locator, locator_type="id"):
+        element = None
+        try:
+            locator_type = locator_type.lower()
+            by_type = self.enter_by_type(locator_type)
+            element = self.driver.find_element(by_type, locator)
+            print("The specified item was found.")
+        except:
+            print("The specified item was not found.")
+        return element
+
--- tests/utilities/using_wrappers.py
+++ tests/utilities/using_wrappers.py
@@ -2,21 +2,27 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from tests.utilities.helpers import Helpers
 from paths import Paths
 
 
-class UsingWrappers:
-    def __init__(self):
-        self.driver = webdriver.Chrome(executable_path=Paths.CHROMEDRIVER_PATH)
-        self.driver.maximize_window()
-        self.page = "https://courses.letskodeit.com/practice"
-        self.driver.implicitly_wait(5)
-        self.driver.get(self.page)
+class UsingHelpers:
 
     def test(self):
+        page = "https://courses.letskodeit.com/practice"
+        driver = webdriver.Chrome(executable_path=Paths.CHROMEDRIVER_PATH)
+        driver.maximize_window()
+        driver.implicitly_wait(5)
+        he = Helpers(driver)
+        driver.get(page)
 
-        self.driver.close()
+        find_item_1 = he.enter_by_type("name")
+        find_item_1.send_keys("Test")
+        time.sleep(3)
+        find_item_2 = he.by_element("//input[@id='name']|", locator_type="xpath")
+        find_item_2.clear()
+        driver.close()
 
 
-run_test_using_wrappers = UsingWrappers()
-run_test_using_wrappers.test()
+run_test_using_helpers = UsingHelpers()
+run_test_using_helpers.test()
