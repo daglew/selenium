72ac3c50eb7df13a2b4dba89d7c3c89dde109359
daglew <daga122611@gmail.com>
2022-12-03 00:07:55 +0100

    selector calendar

--- tests/advanced/selector_calendar.py
+++ tests/advanced/selector_calendar.py
@@ -1,6 +1,7 @@
 import time
-
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 from paths import Paths
 
 
@@ -15,17 +16,31 @@ class CalendarSelector:
     def test(self):
         self.driver.find_element_by_css_selector(css_selector="[aria-controls='wizard-flight-pwa'] .uitk-tab-text")
         # starting field
-        self.driver.find_element_by_class_name(name="#location-field-leg1-origin-menu .uitk-form-field-trigger")
+        departure_dates = self.driver.find_element_by_id(id_="d1-btn")
+        departure_dates.click()
+        # day selection
+        day_selection = self.driver.find_element_by_xpath(xpath="//tbody/tr[4]/td[6]/button[@type='button']")
+        day_selection.click()
 
+        time.sleep(2)
+        self.driver.close()
 
+    def test2(self):
+        # flight card
+        self.driver.find_element_by_css_selector(css_selector="[aria-controls='wizard-flight-pwa'] .uitk-tab-text")
+        # starting field
+        departure_dates = self.driver.find_element_by_id(id_="d1-btn")
+        departure_dates.click()
 
-
-
-
-        time.sleep(5)
+        calendar = self.driver.find_elements(By.XPATH, "//div[@class='uitk-date-picker-menu-months-container']/div[1]//button[@class='uitk-date-picker-day']")
+        for element in calendar:
+            if element.get_attribute(name="data-day") == "25":
+                element.click()
+        time.sleep(2)
         self.driver.close()
 
 
 run_test_calendar_selector = CalendarSelector()
 run_test_calendar_selector.test()
+# run_test_calendar_selector.test2()
 
