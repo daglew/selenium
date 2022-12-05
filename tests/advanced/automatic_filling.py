efa8ed0909c17c4ddd4bc2306ad1501de1fca810
daglew <daga122611@gmail.com>
2022-12-05 23:36:37 +0100

    selector calendar 3

--- tests/advanced/automatic_filling.py
+++ tests/advanced/automatic_filling.py
@@ -12,13 +12,34 @@ class AutomaticFiling:
         self.driver.implicitly_wait(2)
         self.driver.get(self.page)
 
-    def automatic_hint(self):
+    def test_automatic_hint(self):
+        time.sleep(3)
         air_ticket = self.driver.find_element(By.XPATH, "//span[@title='Bilety lotnicze']")
         air_ticket.click()
+        time.sleep(3)
 
-        field_to_enter = self.driver.find_element(By.ID, "field to enter")
+        target_place = "Honolulu, Stany Zjednoczone - Lotnisko Honolulu [HNL]"
+
+        field_to_enter = self.driver.find_element(By.ID, "MultisearchesTo1")
         field_to_enter.click()
         field_to_enter.send_keys("Hon")
 
-        target_place = "Honolulu, Stany Zjednoczone - Lotnisko Honolulu [HNL]"
+        hint_frame = self.driver.find_element(By.ID, "autocomplete_results")
+        inner_html = hint_frame.get_attribute("innerHTML")
+        # print(inner_html)
+
+        list_elements = hint_frame.find_elements(By.TAG_NAME, "li")
+        time.sleep(3)
+
+        for element in list_elements:
+            if target_place in element.text:
+                element.click()
+                break
+
+        time.sleep(2)
+        self.driver.close()
+
+
+run_automatic_filing = AutomaticFiling()
+run_automatic_filing.test_automatic_hint()
 
