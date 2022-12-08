cb9605cab5e3065f2b9728a81b7c671db0529679
daglew <daga122611@gmail.com>
2022-12-08 23:37:09 +0100

    selector calendar 5

---
+++ tests/advanced/java_script_execution.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from paths import Paths
+
+
+class JavaScript:
+    def __init__(self):
+        self.driver = webdriver.Chrome(executable_path=Paths.CHROMEDRIVER_PATH)
+        self.driver.maximize_window()
+        # self.page = "https://courses.letskodeit.com/practice"
+        # self.driver.get(self.page)
+        self.driver.execute_script("window.location = 'https://courses.letskodeit.com/practice';")
+        self.driver.implicitly_wait(3)
+
+    def test_java_script_execute(self):
+        input_your_name = self.driver.execute_script("return document.getElementById('name');")
+        input_your_name.send_keys("Test.")
+
+
+run_test_java_script = JavaScript()
+run_test_java_script.test_java_script_execute()
+
