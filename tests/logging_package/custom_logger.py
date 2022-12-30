917cd4b617d7785a7da09b2ad239ab42032a2b40
daglew <daga122611@gmail.com>
2022-12-30 23:53:52 +0100

    logger custom 1

--- tests/logging_package/custom_logger.py
+++ tests/logging_package/custom_logger.py
@@ -1,5 +1,8 @@
 import inspect
 import logging
+import os
+import pathlib
+import shutil
 
 
 def custom_logger(log_level):
@@ -15,8 +18,26 @@ def custom_logger(log_level):
                                   datefmt='%d/%m/%Y %H:%M:%S %p')
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
+    return logger, file_handler
 
-    return logger
 
 def delete_logs_after_execution():
+    path = pathlib.Path("D:/repositories/selenium/tests/logging_package/logs")
+    list_files = os.listdir(path)
+    for element in list_files:
+        if element[-4:] == ".log":
+            try:
+                os.remove(pathlib.Path(f"{path}/{element}"))
+            except OSError as oe:
+                print(f"Error: {oe} file deletion failed.")
+        else:
+            print(f"Element: {element} is not finished with .log extension.")
+
+    #
+    #files = [os.remove(pathlib.Path(f"{path}/{element}")) for element in list_files if element[-4:] == ".log"]
+
+
+
+
+
 
