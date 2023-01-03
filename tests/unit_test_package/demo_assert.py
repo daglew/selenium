c00710fad1d2cd2d104cc67cd59f1162f2b5ddae
daglew <daga122611@gmail.com>
2023-01-03 22:58:51 +0100

    test case 2

---
+++ tests/unit_test_package/demo_assert.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+class DemoAssert(unittest.TestCase):
+
+    def test_assertTrueFalse(self):
+        a = True
+        self.assertTrue(a, "a is not True")
+        b = False
+        self.assertFalse(b, "b is not False")
+
+    def test_assertEqual(self):
+        a = "Test"
+        b = "Test"
+        self.assertEqual(a, b, msg="'a' is not equal to 'b'")
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
