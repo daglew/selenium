78cf02b7fbde8d67f93676ec95eff57daf8cd578
daglew <daga122611@gmail.com>
2023-01-04 23:38:37 +0100

    test case 3

--- tests/unit_test_package/demo_assert.py
+++ tests/unit_test_package/demo_assert.py
@@ -5,9 +5,9 @@ class DemoAssert(unittest.TestCase):
 
     def test_assertTrueFalse(self):
         a = True
-        self.assertTrue(a, "a is not True")
+        self.assertTrue(a, "a is not False")
         b = False
-        self.assertFalse(b, "b is not False")
+        self.assertFalse(b, "b is not True")
 
     def test_assertEqual(self):
         a = "Test"
