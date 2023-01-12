1c812a798abcc19fb79d89a2f1652418373038ab
daglew <daga122611@gmail.com>
2023-01-12 22:49:21 +0100

    pytest 3

--- tests/pytest_package/conftest.py
+++ tests/pytest_package/conftest.py
@@ -1,8 +1,16 @@
 import pytest
 
+
 @pytest.fixture
 def setUp():
     print("Runing conftest demo1 before every method setup.")
     yield
     print("Runing conftest demo1 before every method tearDown.")
+
+
+@pytest.fixture(scope="module")
+def one_time_setUp():
+    print("Runing conftest demo_one_time before every method setup.")
+    yield
+    print("Runing conftest demo_one_time before every method tearDown.")
 
