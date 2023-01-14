2de84a574eefaa28db6a07583a0787e13940e0cd
daglew <daga122611@gmail.com>
2023-01-14 23:55:53 +0100

    pytest 6

--- tests/pytest_package/conftest.py
+++ tests/pytest_package/conftest.py
@@ -1,16 +1,44 @@
 import pytest
 
 
-@pytest.fixture
+# @pytest.fixture
+# def setup():
+#     print("Runing conftest demo1 before every method setup.")
+#     yield
+#     print("Runing conftest demo1 before every method teardown.")
+#
+#
+# @pytest.fixture(scope="module")
+# def one_time_setup():
+#     print("Runing conftest demo_one_time before every method setup.")
+#     yield
+#     print("Runing conftest demo_one_time before every method teardown.")
+#
+
+@pytest.fixture()
 def setup():
-    print("Runing conftest demo1 before every method setup.")
+    print("Running method level setup.")
     yield
-    print("Runing conftest demo1 before every method teardown.")
+    print("Running method level teardown.")
 
 
 @pytest.fixture(scope="module")
-def one_time_setup():
-    print("Runing conftest demo_one_time before every method setup.")
+def one_time_setup(browser, ostype):
+    print("Running one time setup.")
+    if browser == "firefox":
+        print("Running tests on FF.")
+    else:
+        print("Running tests on chrome.")
     yield
-    print("Runing conftest demo_one_time before every method teardown.")
+    print("Running one time teardown.")
+
+
+def pytest_add_option(parser):
+    parser.addoption("--browser")
+    parser.addoption("--ostype", help="Type of operating system.")
+
+
+@pytest.fixture(scope="module")
+def browser(request):
+    return request.config.getoption("--browser")
 
