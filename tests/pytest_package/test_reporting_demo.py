6367887fb532e2025e3355695ffd4545b4e25eae
daglew <daga122611@gmail.com>
2023-01-18 23:12:45 +0100

    pytest 10

---
+++ tests/pytest_package/test_reporting_demo.py
@@ -0,0 +1,22 @@
+import pytest
+
+from tests.pytest_package.class_to_test import SomeClassToTest
+
+
+@pytest.mark.usefixtures("one_time_setup", "setup")
+class TestReportingDemo:
+
+    @pytest.fixture(autouse=True)
+    def class_setup(self):
+        self.abc = SomeClassToTest(value=10)
+
+    def test_methoda(self):
+        result = self.abc.sum_numbers(2, 8)
+        assert result == 20
+        print("Running method a.")
+
+    def test_methodb(self):
+        result = self.abc.sum_numbers(2, 8)
+        assert result > 20
+        print("Running method b.")
+
